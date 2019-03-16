d9b453f2d471181b5cf3746f992a309a2688d042
Mariana Garman <garman.mariana@gmail.com>
2019-03-16 13:10:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -34,26 +34,3 @@ def retrieve_last_element_from_array(array)
   ["wow", "I", "am", "really", "learning", "arrays!"][5]
 end
 
-def add_element_to_start_of_array(array, element)
-  
-end
-
-def remove_element_from_end_of_array(array)
-  
-end
-
-def remove_element_from_start_of_array(array)
-  
-end
-
-def retrieve_element_from_index(array, index_number)
-  
-end
-
-def retrieve_first_element_from_array(array)
-  
-end
-
-def retrieve_last_element_from_array(array)
-  
-end
